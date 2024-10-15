238d5f82ac941416a70e13d01c90909154134a0f
Saru <heo_dark@naver.com>
2024-10-15 17:09:35 +0900

    chore: db, jpa, entity 설정 변경

--- src/main/java/hhplus/booking/ConcertBookingApplication.java
+++ src/main/java/hhplus/booking/ConcertBookingApplication.java
@@ -2,6 +2,8 @@ package hhplus.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class ConcertBookingApplication {
@@ -9,5 +11,5 @@ public class ConcertBookingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConcertBookingApplication.class, args);
     }
-
 }
+

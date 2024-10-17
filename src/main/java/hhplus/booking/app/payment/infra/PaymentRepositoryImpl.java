7b47464c30d46b9a1a23ea3255597772c1e59dbb
Saru <heo_dark@naver.com>
2024-10-17 17:48:45 +0900

    refactor: import 제거

--- src/main/java/hhplus/booking/app/payment/infra/PaymentRepositoryImpl.java
+++ src/main/java/hhplus/booking/app/payment/infra/PaymentRepositoryImpl.java
@@ -3,14 +3,8 @@ package hhplus.booking.app.payment.infra;
 import hhplus.booking.app.payment.domain.entity.Payment;
 import hhplus.booking.app.payment.domain.repository.PaymentRepository;
 import hhplus.booking.app.payment.infra.jpa.PaymentJpaRepository;
-import hhplus.booking.app.queue.domain.entity.Queue;
-import hhplus.booking.app.queue.domain.repository.QueueRepository;
-import hhplus.booking.app.queue.infra.jpa.QueueJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor

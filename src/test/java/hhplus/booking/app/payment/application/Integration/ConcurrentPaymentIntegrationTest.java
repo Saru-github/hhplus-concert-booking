689fbd5c566ac5c0491c4260e4ea4b01f788188b
Saru <heo_dark@naver.com>
2024-10-31 16:48:34 +0900

    test: 테스트 케이스 비관 - 낙관 정리

--- src/test/java/hhplus/booking/app/payment/application/Integration/ConcurrentPaymentIntegrationTest.java
+++ src/test/java/hhplus/booking/app/payment/application/Integration/ConcurrentPaymentIntegrationTest.java
@@ -1,5 +1,6 @@
 package hhplus.booking.app.payment.application.Integration;
 
+import hhplus.booking.app.ExecutionTimeWatcher;
 import hhplus.booking.app.concert.infra.jpa.ConcertBookingJpaRepository;
 import hhplus.booking.app.payment.application.PaymentUseCase;
 import hhplus.booking.app.payment.application.dto.PaymentInfo;
@@ -7,6 +8,7 @@ import hhplus.booking.app.payment.infra.jpa.PaymentJpaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,25 +21,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
+@ExtendWith(ExecutionTimeWatcher.class)
 class ConcurrentPaymentIntegrationTest {
 
     @Autowired
     private PaymentUseCase paymentUseCase;
 
     @Test
-    @DisplayName("[성공] 예약된 상태인 1번 예약에 관하여 정상적으로 1건만 최종 예약성공")
-    void testPaymentUseCaseTest() throws Exception {
+    @DisplayName("[비관적락_성공] 예약된 상태인 1번 예약에 관하여 정상적으로 1건만 최종 결제성공")
+    void pessimisticLockTestPaymentUseCaseTest() throws Exception {
 
         // Given
         List<CompletableFuture<PaymentInfo.Output>> futures = new ArrayList<>();
         AtomicLong atomicExceptionCount = new AtomicLong(0L);
 
         // When
-        for (int i = 0; i < 10; i++) {
-            int finalI = i;
+        for (int i = 0; i < 1000; i++) {
             CompletableFuture<PaymentInfo.Output> future = CompletableFuture.supplyAsync(() -> {
                 try {
-                    log.info("시도: {}", finalI);
                     return paymentUseCase.processPayment(new PaymentInfo.Input(1L, "Bearer TEST_UUID_TOKEN"));
                 } catch (Exception e) {
                     atomicExceptionCount.getAndIncrement();
@@ -63,6 +64,50 @@ class ConcurrentPaymentIntegrationTest {
 
         // 결과 확인
         assertThat(results).hasSize(1);
-        assertThat(exceptionCount).isEqualTo(9);
+        assertThat(exceptionCount).isEqualTo(999);
+
+        log.info("결제성공 건: {}건, 결제실패 건: {}건", results.size(), exceptionCount);
+    }
+
+    @Test
+    @DisplayName("[비관적락_성공] 예약된 상태인 1번 예약에 관하여 정상적으로 1건만 최종 결제성공")
+    void optimisticLockTestPaymentUseCaseTest() throws Exception {
+
+        // Given
+        List<CompletableFuture<PaymentInfo.Output>> futures = new ArrayList<>();
+        AtomicLong atomicExceptionCount = new AtomicLong(0L);
+
+        // When
+        for (int i = 0; i < 1000; i++) {
+            CompletableFuture<PaymentInfo.Output> future = CompletableFuture.supplyAsync(() -> {
+                try {
+                    return paymentUseCase.processPayment(new PaymentInfo.Input(1L, "Bearer TEST_UUID_TOKEN"));
+                } catch (Exception e) {
+                    atomicExceptionCount.getAndIncrement();
+                    return null;
+                }
+            });
+            futures.add(future);
+        }
+
+        // 모든 CompletableFuture가 완료될 때까지 대기
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        allOf.get();
+
+        // Then
+        List<PaymentInfo.Output> results = new ArrayList<>();
+        for (CompletableFuture<PaymentInfo.Output> future : futures) {
+            PaymentInfo.Output output = future.join();
+            if (output != null) {
+                results.add(output);
+            }
+        }
+        long exceptionCount = atomicExceptionCount.get();
+
+        // 결과 확인
+        assertThat(results).hasSize(1);
+        assertThat(exceptionCount).isEqualTo(999);
+
+        log.info("결제성공 건: {}건, 결제실패 건: {}건", results.size(), exceptionCount);
     }
 }
--- src/test/java/hhplus/booking/app/user/application/Integration/ConcurrentUserIntegrationTest.java
+++ src/test/java/hhplus/booking/app/user/application/Integration/ConcurrentUserIntegrationTest.java
@@ -1,5 +1,6 @@
 package hhplus.booking.app.user.application.Integration;
 
+import hhplus.booking.app.ExecutionTimeWatcher;
 import hhplus.booking.app.user.application.UserService;
 import hhplus.booking.app.user.application.dto.UserPointInfo;
 import hhplus.booking.app.user.domain.entity.User;
@@ -7,6 +8,7 @@ import hhplus.booking.app.user.infra.jpa.UserJpaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
+@ExtendWith(ExecutionTimeWatcher.class)
 class ConcurrentUserIntegrationTest {
 
     @Autowired
@@ -28,20 +31,26 @@ class ConcurrentUserIntegrationTest {
 
 
     @Test
-    @DisplayName("[성공] 새로생성한 4번 유저의 10번의 10000포인트 요청시, 100000만 포인트 올바르게 충전")
-    void testPaymentUseCaseTest() throws Exception {
+    @DisplayName("[낙관적_성공] 새로생성한 4번 유저의 1000번의 10000포인트 요청시, 10000000만 포인트 올바르게 충전")
+    void pessimisticLockTestUserPointUserTest() throws Exception {
+
 
         // Given
-
         userJpaRepository.save(User.of("대영"));
         UserPointInfo.Output beforeUserPoint = userService.getUserPoints(new UserPointInfo.Input(4L, null));
+
 
         List<CompletableFuture<UserPointInfo.Output>> futures = new ArrayList<>();
 
         // When
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000; i++) {
             CompletableFuture<UserPointInfo.Output> future = CompletableFuture.supplyAsync(() -> {
-                return userService.chargeUserPoints(new UserPointInfo.Input(4L, 10000L));
+                try {
+                    return userService.chargeUserPoints(new UserPointInfo.Input(4L, 10000L));
+                } catch (Exception e) {
+                    e.getStackTrace();
+                    return null;
+                }
             });
             futures.add(future);
         }
@@ -62,7 +71,54 @@ class ConcurrentUserIntegrationTest {
 
         // 결과 확인
         assertThat(beforeUserPoint.balance()).isEqualTo(0L);
-        assertThat(afterUserPoint.balance()).isEqualTo(100000L);
+        assertThat(afterUserPoint.balance()).isEqualTo(10000000L);
+
+        log.info("테스트 시작 전 포인트: {}", beforeUserPoint.balance());
+        log.info("테스트 시작 후 포인트: {}", afterUserPoint.balance());
+    }
+
+    @Test
+    @DisplayName("[비관적락_성공] 새로생성한 4번 유저의 1000번의 10000포인트 요청시, 10000000만 포인트 올바르게 충전")
+    void optimisticLockTestUserPointUserTest() throws Exception {
+
+
+        // Given
+        userJpaRepository.save(User.of("대영"));
+        UserPointInfo.Output beforeUserPoint = userService.getUserPoints(new UserPointInfo.Input(4L, null));
+
+
+        List<CompletableFuture<UserPointInfo.Output>> futures = new ArrayList<>();
+
+        // When
+        for (int i = 0; i < 1000; i++) {
+            CompletableFuture<UserPointInfo.Output> future = CompletableFuture.supplyAsync(() -> {
+                try {
+                    return userService.chargeUserPoints(new UserPointInfo.Input(4L, 10000L));
+                } catch (Exception e) {
+                    e.getStackTrace();
+                    return null;
+                }
+            });
+            futures.add(future);
+        }
+
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        allOf.get();
+
+        // Then
+        List<UserPointInfo.Output> results = new ArrayList<>();
+        for (CompletableFuture<UserPointInfo.Output> future : futures) {
+            UserPointInfo.Output output = future.join();
+            if (output != null) {
+                results.add(output);
+            }
+        }
+
+        UserPointInfo.Output afterUserPoint = userService.getUserPoints(new UserPointInfo.Input(4L, null));
+
+        // 결과 확인
+        assertThat(beforeUserPoint.balance()).isEqualTo(0L);
+        assertThat(afterUserPoint.balance()).isEqualTo(10000000L);
 
         log.info("테스트 시작 전 포인트: {}", beforeUserPoint.balance());
         log.info("테스트 시작 후 포인트: {}", afterUserPoint.balance());

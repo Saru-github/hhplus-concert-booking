9718f53f85b54253c1a0ab471f82357e69f54c7b
Saru <heo_dark@naver.com>
2024-11-12 19:19:42 +0900

    chore: 스케쥴러 임시 비활성

--- src/main/java/hhplus/booking/config/web/SchedulerConfig.java
+++ src/main/java/hhplus/booking/config/web/SchedulerConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableScheduling // 스케쥴러 활성화
+// @EnableScheduling // 스케쥴러 활성화
 public class SchedulerConfig {
 }

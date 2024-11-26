66274579855b77678ec8963bc725851686809964
Saru <heo_dark@naver.com>
2024-11-26 16:43:34 +0900

    feat: k6 설정 재추가 및 변경

--- src/main/resources/script.js
+++ src/main/resources/script.js
@@ -1,17 +1,53 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
+import { group } from 'k6';
 
 export const options = {
-  vus: 1000,
-  duration: '10s',
+  scenarios: {
+    load_test: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        { duration: '1m', target: 2500 },
+        { duration: '2m', target: 2500 },
+        { duration: '30s', target: 0 },
+      ],
+    },
+    endurance_test: {
+      executor: 'constant-vus',
+      vus: 3000,
+      duration: '3m',
+      startTime: '4m',
+    },
+    stress_test: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        { duration: '1m', target: 3000 },
+        { duration: '1m', target: 4000 },
+        { duration: '1m', target: 5000 },
+        { duration: '30s', target: 0 },
+      ],
+      startTime: '7m30s',
+    },
+    peak_test: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        { duration: '30s', target: 2500 },
+        { duration: '30s', target: 5000 },
+        { duration: '1m', target: 5000 },
+        { duration: '30s', target: 0 },
+      ],
+      startTime: '11m30s',
+    },
+  },
 };
 
 export default function () {
-  const res = http.get('http://host.docker.internal:8081/booking/queue/status', {
-    headers: {
-      Authorization: 'Bearer TEST_UUID_TOKEN', // 필요시 Authorization 헤더 추가
-    },
+  group('API Test', function () {
+    const res = http.get('http://host.docker.internal:8080/booking/queue/status');
+    check(res, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
   });
-  console.log(`Response status: ${res.status}`);
-  sleep(1);
 }
